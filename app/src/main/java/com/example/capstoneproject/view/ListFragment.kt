4f8066746d7aa7d986458c45f0c93fc98548a223
Kerem Mimir <mimirkerem@gmail.com>
2024-08-21 18:43:32 +0300

    CapstoneProect | Refactor list screen

--- app/src/main/java/com/example/capstoneproject/view/ListFragment.kt
+++ app/src/main/java/com/example/capstoneproject/view/ListFragment.kt
@@ -42,59 +42,44 @@ class ListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        adapter = MoviesAdapter(dataList, viewModel)
-        binding.recylerView.adapter = adapter
+        val type = args.type
+        viewModel.fetchData(type)
 
-        val listName = args.list
-
-        observeData(listName)
-
-        binding.backHome.setOnClickListener {
-            val action = ListFragmentDirections.actionListFragmentToHomeFragment()
-            findNavController().navigate(action)
-        }
+        observeData()
+        setupViews()
     }
 
-    private fun observeData(listName: String) {
-        if (listName == "movies") {
-            viewModel.movies.observe(viewLifecycleOwner, Observer { movies ->
-                Log.d("ListFragment", "Movies LiveData updated")
-                adapter.updateData(movies)
+    private fun observeData() {
+        viewModel.movies.observe(viewLifecycleOwner, Observer { movies ->
+            adapter.updateData(movies)
+        })
 
-                adapter.onClick = { data ->
-                    val action = ListFragmentDirections.actionListFragmentToDetailFragment(data)
-                    findNavController().navigate(action)
-                }
-            })
-            if (viewModel.movies.value.isNullOrEmpty()) {
-                Log.d("ListFragment", "Fetching movies data")
-                viewModel.getMovies()
-            } else {
-                Log.d("ListFragment", "Movies data already loaded")
-            }
-        } else if (listName == "series") {
-            viewModel.series.observe(viewLifecycleOwner, Observer { series ->
-                Log.d("ListFragment", "Series LiveData updated")
-                adapter.updateData(series)
-
-                adapter.onClick = { data ->
-                    val action = ListFragmentDirections.actionListFragmentToDetailFragment(data)
-                    findNavController().navigate(action)
-                }
-            })
-            if (viewModel.series.value.isNullOrEmpty()) {
-                Log.d("ListFragment", "Fetching series data")
-                viewModel.getSeries()
-            } else {
-                Log.d("ListFragment", "Series data already loaded")
-            }
-        }
+        viewModel.series.observe(viewLifecycleOwner, Observer { series ->
+            adapter.updateData(series)
+        })
 
         viewModel.error.observe(viewLifecycleOwner, Observer { errorMessage ->
             errorMessage?.let {
                 Log.e("ListFragment", "Error: $it")
             }
         })
+    }
+    private fun setupViews(){
+        adapter = MoviesAdapter(dataList, viewModel)
+        binding.recylerView.adapter = adapter
+
+        adapter.onClick = { data ->
+            val action = ListFragmentDirections.actionListFragmentToDetailFragment(data)
+            findNavController().navigate(action)
+        }
+        adapter.onClick = { data ->
+            val action = ListFragmentDirections.actionListFragmentToDetailFragment(data)
+            findNavController().navigate(action)
+        }
+        binding.backHome.setOnClickListener {
+            val action = ListFragmentDirections.actionListFragmentToHomeFragment()
+            findNavController().navigate(action)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/capstoneproject/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/capstoneproject/viewmodel/ListViewModel.kt
@@ -4,53 +4,58 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.capstoneproject.model.DataModel
+import com.example.capstoneproject.model.Type
 import com.example.capstoneproject.repository.SharedPreferencesRepository
 import com.example.capstoneproject.service.IMDbMoviesAPI
 import com.example.capstoneproject.service.IMDbMoviesAPIService
 import com.example.capstoneproject.service.response.toDataModel
 import kotlinx.coroutines.launch
 
-class ListViewModel(private val sharedPreferencesRepository: SharedPreferencesRepository) : ViewModel() {
+class ListViewModel(private val sharedPreferencesRepository: SharedPreferencesRepository) :
+    ViewModel() {
     private val imdbMoviesAPI: IMDbMoviesAPI = IMDbMoviesAPIService.getIMDbAPI()
     val movies = MutableLiveData<List<DataModel>>()
     val series = MutableLiveData<List<DataModel>>()
     val favorites = MutableLiveData<List<DataModel>>()
     val error = MutableLiveData<String>()
 
-    init {
-        getSeries()
-        getMovies()
+    fun fetchData(type: Type) {
+        when (type) {
+            Type.MOVIES -> getMovies()
+            Type.SERIES -> getSeries()
+        }
     }
-    fun getMovies(){
-        if (movies.value.isNullOrEmpty()){
+
+    private fun getMovies() {
+        if (movies.value.isNullOrEmpty()) {
             viewModelScope.launch {
                 try {
                     val response = imdbMoviesAPI.getMovies()
-                    if (response.isSuccessful){
+                    if (response.isSuccessful) {
                         movies.value = response.body()?.map { it.toDataModel() }
                         updateFavorites()
-                    }else{
+                    } else {
                         error.value = "Error : ${response.message()}"
                     }
-                }catch (e : Exception){
+                } catch (e: Exception) {
                     error.value = e.message
                 }
             }
         }
     }
 
-    fun getSeries(){
+    private fun getSeries() {
         if (series.value.isNullOrEmpty()) {
             viewModelScope.launch {
                 try {
                     val response = imdbMoviesAPI.getSeries()
-                    if (response.isSuccessful){
+                    if (response.isSuccessful) {
                         series.value = response.body()?.map { it.toDataModel() }
                         updateFavorites()
-                    }else{
+                    } else {
                         error.value = "Error : ${response.message()}"
                     }
-                }catch (e : Exception){
+                } catch (e: Exception) {
                     error.value = e.message
                 }
             }
@@ -64,7 +69,7 @@ class ListViewModel(private val sharedPreferencesRepository: SharedPreferencesRe
         }
     }
 
-    fun updateFavorites() {
+   private fun updateFavorites() {
         val favoriteIds = sharedPreferencesRepository.getFavoriteIds()
         val combinedList = (movies.value ?: emptyList()) + (series.value ?: emptyList())
         val favoriteList = combinedList.filter { it.imdbId in favoriteIds }
